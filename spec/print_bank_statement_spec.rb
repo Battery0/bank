54f2ffa66a45230253b323d8af0f07c9da213bca
ian k <kickthechair@gmail.com>
2021-09-28 15:12:42 +0100

    test passing for PrintBankStatement correctly returns the statement headers

--- spec/print_bank_statement_spec.rb
+++ spec/print_bank_statement_spec.rb
@@ -1,21 +1,20 @@
-# require 'print_bank_statement'
+require 'print_bank_statement'
 
-# describe PrintBankStatement do
+describe PrintBankStatement do
 
-#   let(:print_bank_statement) { described_class.new }
-#   #let(:account_double) { double("account double") }
-#   let(:date_of_transaction) { Time.now.strftime("%d/%m/%Y") }
-#   let(:transaction_history) { ["#{date_of_transaction} || £500.00 ||  || £500.00"] }
+  let(:print_bank_statement) { described_class.new }
+  let(:headers) { "date || credit || debit || balance" }
+  let(:transaction_history) { ["#{Time.now.strftime("%d/%m/%Y")} || £500.00 ||  || £500.00"] }
 
 
-#   describe '#print_statement' do
-#     it 'should output the required headers' do
-#       expect(print_bank_statement.print_statement(transaction_history)).to eq("date_of_transaction || £500.00 ||  || £500.00")
-#     end
+  describe '#print_statement' do
+    it 'should output the required headers' do
+      expect(print_bank_statement.print_statement(transaction_history)).to eq(headers)
+    end
 
-#     # it 'should output the bank statement transaction history' do
-#     #   expect(print_bank_statement.print_statement).to eq("#{date_of_transaction} || £500.00 ||  || £500.00")
-#     # end
-#   end
+    # it 'should output the bank statement transaction history' do
+    #   expect(print_bank_statement.print_statement).to eq("#{date_of_transaction} || £500.00 ||  || £500.00")
+    # end
+  end
 
-# end
+end
