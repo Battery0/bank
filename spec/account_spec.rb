e3dd5e78c87bfe724a48d3268bcaaf1c9f853af6
ian k <kickthechair@gmail.com>
2021-10-01 16:07:52 +0100

    doubles working correctly for Account credit method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,13 +3,16 @@ require 'account'
 describe Account do
 
   let(:account) { described_class.new }
-  let(:date_of_transaction) { Time.now.strftime("%d/%m/%Y") }
+  let(:date) { Time.now.strftime("%d/%m/%Y") }
   let(:credit) { "£500.00" }
   let(:balance_credit) { "£500.00" }
   let(:debit) { "£65.00" }
   let(:balance_debit) { "£435.00" }
   let(:print_bank_statement_dbl) { double("print statement double", { print_statement: "date || credit || debit || balance" }) }
-  let(:transaction_dbl) { double("transaction double") }
+  let(:formatted_statement) { "#{date} || £500.00 ||  || £500.00" }
+  let(:credit_transaction_dbl) { double("transaction double", { credit: { date: date, credit: "£500.00", debit: "", current_balance: "£500.00" } }) }
+
+
 
   describe '#credit' do
     it 'raises error if credit is not a Float or Integer' do
@@ -17,9 +20,8 @@ describe Account do
     end
 
     it 'updates the account history with a credit transaction' do
-      account.credit(500)
-      # expect(account.account_statement).to
-      # expect(account.transaction_history).to include(transaction_dbl)
+      expect(credit_transaction_dbl).to receive(:credit).with(500, "£500.00")
+      account.credit(500, credit_transaction_dbl)
     end
 
     it 'shows the correct account balance after depositing money into the account' do
@@ -27,22 +29,37 @@ describe Account do
     end
   end
 
-  describe '#debit' do
-    it 'raises error if trying to debit more money than currently available in the balance' do
-      expect { account.debit(1) }.to raise_error("Your account does not currently have that much money.")
-    end
 
-    it 'updates the account history with a debit transaction' do
-      account.credit(500)
-      account.debit(65)
-      # expect(account.transaction_history).to include(transaction_dbl)
-    end
 
-    it 'shows the correct account balance after withdrawing money from the account' do
-      account.credit(500)
-      expect(account.debit(65)).to eq(balance_debit)
-    end
-  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # describe '#debit' do
+  #   it 'raises error if trying to debit more money than currently available in the balance' do
+  #     expect { account.debit(1) }.to raise_error("Your account does not currently have that much money.")
+  #   end
+
+  #   it 'updates the account history with a debit transaction' do
+  #     account.credit(500)
+  #     account.debit(65)
+  #     # expect(account.transaction_history).to include(transaction_dbl)
+  #   end
+
+  #   it 'shows the correct account balance after withdrawing money from the account' do
+  #     account.credit(500)
+  #     expect(account.debit(65)).to eq(balance_debit)
+  #   end
+  # end
 
   describe '#account_statement' do
     it 'outputs the account transaction history statement' do
