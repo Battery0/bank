316b21c6f9e7611a4ff00b56f423c2f789bc687f
ian k <kickthechair@gmail.com>
2021-10-01 14:50:39 +0100

    cremoved whitespace in bank_statement_printer_spec.rb

--- spec/bank_statement_printer_spec.rb
+++ spec/bank_statement_printer_spec.rb
@@ -5,17 +5,8 @@ describe BankStatementPrinter do
   let(:print_bank_statement) { described_class.new }
   let(:statement_header) { "date || credit || debit || balance" }
   let(:date) { Time.now.strftime("%d/%m/%Y") }
-
   let(:transaction_dbl) { double("transaction double", { record: { date: date, credit: "£500.00", debit: "", current_balance: "£500.00" } }) }
-
-
-
-
   let(:transaction_history) { [transaction_dbl] }
-
-  
-
-
   let(:formatted_statement) { "#{date} || £500.00 ||  || £500.00" }
 
   describe '#print_statement' do
