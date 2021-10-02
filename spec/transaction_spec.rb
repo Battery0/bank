e2ab96c05d97a1e755c0b280a97458041733c56f
ian k <kickthechair@gmail.com>
2021-10-02 16:28:21 +0100

    all tests passing and isolated from external dependancies

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,20 +4,16 @@ describe Transaction do
 
   let(:transaction) { described_class.new }
   let(:date) { Time.now.strftime("%d/%m/%Y") }
-  let(:credit) { "£500.00" }
-  let(:balance_credit) { "£500.00" }
-  let(:debit) { "£65.00" }
-  let(:balance_debit) { "£435.00" }
 
   describe '#credit_transaction' do
     it 'creates a transaction record when the account is credited' do
-      expect(transaction.credit(500, balance_credit)).to eq({ date: date, credit: credit, debit: "", balance: balance_credit })
+      expect(transaction.credit(500, "£500.00")).to eq({ date: date, credit: "£500.00", debit: "", balance: "£500.00" })
     end
   end
 
   describe '#debit_transaction' do
     it 'creates a transaction record when the account is debited' do
-      expect(transaction.debit(65, balance_debit)).to eq({ date: date, credit: "", debit: debit, balance: balance_debit })
+      expect(transaction.debit(65, "£435.00")).to eq({ date: date, credit: "", debit: "£65.00", balance: "£435.00" })
     end
   end
 
